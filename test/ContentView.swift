c70aa6277327f4e843c58cf5c0b5fb851e63da2b
Jeff <turbo25mike@users.noreply.github.com>
2024-01-10 10:45:56 -0800

    init

---
+++ test/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Jeff K. Johnson on 1/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Button("Open OnX", action: openUrl)
+        }
+        .padding()
+    }
+    
+    func openUrl() {
+        let url = URL(string: "onxhunt://ranged_waypoint")!
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Jeff K. Johnson on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
